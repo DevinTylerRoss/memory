d3cfde83b74fc98f17d22fdbcf3cb60a8e9c29e6
Devin Tyler Ross <devintylerross@gmail.com>
2018-05-22 21:33:20 -0700

    added a lot

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 /*
  * Create a list that holds all of your cards
  */
+
+ /*
+YO ME THIS IS WHAT YOU STILL HAVE TO DO AS OF 5 30 ON SUNDAY
+CREATE A Timer
+CREATE A MODAL POPUP
+CREATE A STAR RATING THING
+ */
 window.onload = function() {
   createCards();
+  //startTimer();
 };
 
 let cardList = ['fa-diamond','fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube',
@@ -12,7 +20,21 @@ let cardList = ['fa-diamond','fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cub
 const deck = document.querySelector(".deck");
 
 
+const startButton = document.getElementById('startButton');
+let openCards = [];
+let score = 0;
+let moves = document.querySelector('.moves');
+let ref_int;
+const restartButton = document.querySelector('.restart');
+let timerRunning = false;
 
+function ifWin(){
+if (score === 8){
+  bringUpModal();
+  stopTimer();
+
+}
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -36,15 +58,13 @@ function shuffle(array) {
 }
 
 
-//console.log(cardList);
-
-const restartButton = document.querySelector('.restart');
-
 function createCards() {
+
   deck.innerHTML = '';
   shuffle(cardList);
   console.log(cardList);
 
+  moves.innerText = 0;
   for(let i = 0; i < cardList.length; i++){
 
     let el = document.createElement("li");
@@ -53,56 +73,139 @@ function createCards() {
     el.appendChild(innerEl);
     el.classList.add('card');
     deck.appendChild(el);
-    /*
-    start with the html
-
-    <li class="card"></li>
-    add inner html of '<li class="fa ' + cardList[i] + '">'
-    */
+    //ref_int = setInterval(timer, 1000);
     console.log(cardList[i]);
   }
 }
 
+//EVENTS
+deck.addEventListener('click', function(event){
+  console.log(timerRunning);
+  if(timerRunning = false){
+    timerRunning = true;
+  startTimer();
+}
+  //puts the event targget in a var
+  const el = event.target;
+  //puts the card you clicked on in the open list array
+  openCards.push(el);
+  moves.innerText++
+  //flips the card over!
+  el.classList.add('open', 'show');
+
+  function noMatch(){
+    //alert('they do not match');
+    openCards[0].classList.remove('open', 'show');
+    openCards[1].classList.remove('open', 'show');
+    openCards = [];
+  }
+
+  if(openCards.length > 1){
+    if(openCards[0].firstElementChild.classList.item(1) === openCards[1].firstElementChild.classList.item(1)){
+
+      openCards[0].classList.add('match');
+      openCards[1].classList.add('match');
+      openCards = [];
+      score += 1;
+      console.log(score);
+      ifWin();
+    } else {
+      setTimeout(noMatch, 1000);
+    }
+  // how to get the symbol > console.log(openCards[0].firstElementChild.classList.item(1));
+function cardLogic(){
+
+
+  }
+
+
+}
+
+});
+
 restartButton.addEventListener('click', createCards);
 
+restartButton.addEventListener('click', function(){
+  stopTimer();
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one) DONE
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-
-//turns over the card
-function addOpenShow(yep){
-  yep.classList.add('open', 'show');
-}
-
-function addToOpenList(){
-  //add clicked card to array of open cards
-
-}
-
-/*
-deck.addEventListener('click', function(event){
-  //open and show class
-  event.target.classList.add('open', 'show');
-  console.log("hey");
 })
+
+//TIMER CODE
+let secDisplay = document.querySelector('.seconds');
+let minDisplay = document.querySelector('.minutes');
+secDisplay.innerText = 00;
+minDisplay.innerText = 00;
+
+function timer(){
+  console.log('gulp');
+  if(secDisplay.innerText == 59){
+    secDisplay.innerText = 00;
+    minDisplay.innerText++
+  }
+
+  secDisplay.innerText++;
+}
+
+function startTimer(){
+
+   ref_int = setInterval(timer, 1000);
+
+}
+
+function stopTimer(){
+  clearInterval(ref_int);
+  timerRunning = false;
+}
+
+startButton.addEventListener('click', function(){
+  startTimer();
+})
+
+restartButton.addEventListener('click', function(){
+
+  clearInterval(ref_int);
+  secDisplay.innerText = 00;
+  minDisplay.innerText = 00;
+});
+
+//MODAL Code
+
+const modal = document.getElementById('myModal');
+const modalButton = document.getElementById('modalTest');
+const giantX = document.querySelector('.giantX');
+const yesButton = document.getElementById('yes-button');
+const totalMoves = document.getElementById('total-moves');
+const totalStars = document.getElementById('total-stars');
+const totalMinutes = document.getElementById('total-minutes');
+const totalSeconds = document.getElementById('total-seconds');
+
+
+
+modalButton.addEventListener('click', function(){
+//  modal.classList.add('modal-visible');
+bringUpModal();
+});
+
+giantX.addEventListener('click', function(){
+  modal.classList.remove('modal-visible');
+});
+
+/*
+window.addEventListener('click', function(){
+    modal.classList.remove('modal-visible');
+});
 */
-deck.addEventListener('click', function(event){
+function bringUpModal(){
+    totalMoves.innerText = moves.innerText;
+    modal.classList.add('modal-visible');
 
-  const el = event.target;
-  //open and show class to card
-  addOpenShow(el);
+}
 
-  //test to see if function is working
-  console.log("hey");
+yesButton.addEventListener('click', function(){
+  modal.classList.remove('modal-visible');
+  createCards();
+  clearInterval(ref_int);
+  secDisplay.innerText = 00;
+  minDisplay.innerText = 00;
+
 })
